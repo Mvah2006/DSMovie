c046af058029e7aa9c328a4ead0cd06c8d842442
Mvah2006 <marcosMV0398@gmail.com>
2023-07-16 22:40:36 -0300

    Project Created

---
+++ backend/src/main/java/com/mvah5527/projeto/dsmovie/ProjetoDsmovieApplication.java
@@ -0,0 +1,13 @@
+package com.mvah5527.projeto.dsmovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoDsmovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoDsmovieApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/mvah5527/projeto/dsmovie/ProjetoDsmovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mvah5527.projeto.dsmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoDsmovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
